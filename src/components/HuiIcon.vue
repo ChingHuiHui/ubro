28311bf9d0bd5779efa6aadb5f67b2689a91f86f
chinghuihui <sunny70624@gmail.com>
2022-03-05 22:44:23 +0800

    fix the props in HuiIcon

--- src/components/HuiIcon.vue
+++ src/components/HuiIcon.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script setup lang="ts">
-  withDefaults(defineProps<{ pack: string; icon: string }>(), {
+  withDefaults(defineProps<{ pack?: string; icon?: string }>(), {
     pack: 'fas',
     icon: '',
   })
