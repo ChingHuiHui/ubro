87075cb00139cd21ae2b6a3654af074fb6a072ca
chinghuihui <sunny70624@gmail.com>
2022-03-29 22:40:57 +0800

    modify the text in admin-login's buttons

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -11,9 +11,9 @@
         <HuiInput name="password" label="Password" type="password" />
         <div class="flex space-x-4">
           <button class="btn btn-outline-primary btn-block" type="button">
-            <router-link class="block" to="/">GoBack</router-link>
+            <router-link class="block" to="/">回使用者登入</router-link>
           </button>
-          <button class="btn btn-primary btn-block" type="submit">Login</button>
+          <button class="btn btn-primary btn-block" type="submit">登入</button>
         </div>
         <div class="mt-1 text-sm text-red-500">{{ errorText }}</div>
       </HuiForm>
